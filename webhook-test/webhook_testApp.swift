4fba96fe48e00158eed00462e92929f6a6c55b8a
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-07-10 23:00:31 +0700

    Initial Commit

---
+++ webhook-test/webhook_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  webhook_testApp.swift
+//  webhook-test
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct webhook_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ webhook-testUITests/webhook_testUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  webhook_testUITestsLaunchTests.swift
+//  webhook-testUITests
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 10/07/23.
+//
+
+import XCTest
+
+final class webhook_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
